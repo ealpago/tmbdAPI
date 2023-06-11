1e69e3b336983bc5382904965331fd7b754c6717
ealpago <alpagoemre@icloud.com>
2023-06-11 16:53:38 +0300

    Initial Commit

---
+++ tmbdAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tmbdAPI
+//
+//  Created by Emre Alpago on 11.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
