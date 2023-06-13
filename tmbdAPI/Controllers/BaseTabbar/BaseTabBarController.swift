29d1f303ab0db9267433ac43aaa33b39921c3bf2
ealpago <alpagoemre@icloud.com>
2023-06-13 23:13:58 +0300

    BaseTabbar is ready

--- tmbdAPI/Controllers/BaseTabbar/BaseTabBarController.swift
+++ tmbdAPI/Controllers/BaseTabbar/BaseTabBarController.swift
@@ -10,23 +10,23 @@ import UIKit
 enum TabBarConstant {
     static let homeStoryBoardName = "HomeStoryboard"
     static let homeStoryBoardID = "HomeViewController"
-    static let addStoryBoardName = "AddStoryboard"
-    static let addStoryBoardID = "AddViewController"
-    static let detailsStoryBoardName = "DetailsStoryboard"
-    static let detailsStoryBoardID = "DetailsViewController"
+    static let upcomingStoryBoardName = "UpcomingStoryboard"
+    static let upcomingStoryBoardID = "UpcomingViewController"
+    static let favoritesStoryBoardName = "FavoritesStoryboard"
+    static let favoritesStoryBoardID = "FavoritesViewController"
 }
 
 enum TabbarItemType {
-    case home, add, pie
+    case home, upcoming, favorites
 
     var name: String {
         switch self {
         case .home:
             return "HomeStoryboard"
-        case .add:
-            return "AddStoryboard"
-        case .pie:
-            return "DetailsStoryboard"
+        case .upcoming:
+            return "UpcomingStoryboard"
+        case .favorites:
+            return "FavoritesStoryboard"
         }
     }
 
@@ -34,38 +34,38 @@ enum TabbarItemType {
         switch self {
         case .home:
             return "HomeViewController"
-        case .add:
-            return "AddViewController"
-        case .pie:
-            return "DetailsViewController"
+        case .upcoming:
+            return "UpcomingViewController"
+        case .favorites:
+            return "FavoritesViewController"
         }
     }
 
     var image: UIImage {
         switch self {
         case .home:
-            return UIImage(named: "timeSVG")!
-        case .add:
-            return UIImage(named: "addSVG")!
-        case .pie:
-            return  UIImage(named: "pieSVG")!
+            return UIImage(systemName: "tv.circle")!
+        case .upcoming:
+            return UIImage(systemName: "clock.badge.checkmark")!
+        case .favorites:
+            return  UIImage(systemName: "star")!
         }
     }
     var selectedImage: UIImage {
         switch self {
         case .home:
-            return UIImage(named: "selectedTimeSVG")!
-        case .add:
-            return UIImage(named: "selectedAddSVG")!
-        case .pie:
-            return UIImage(named: "selectedPieSVG")!
+            return UIImage(systemName: "tv.circle.fill")!
+        case .upcoming:
+            return UIImage(systemName: "clock.badge.checkmark.fill")!
+        case .favorites:
+            return  UIImage(systemName: "star.fill")!
         }
     }
 }
 
 final class BaseTabBarController: UITabBarController {
     var items: [TabbarItemType] {
-        [.home, .add, .pie]
+        [.home, .upcoming, .favorites]
     }
 
     override func viewDidLoad() {
@@ -78,13 +78,10 @@ final class BaseTabBarController: UITabBarController {
     func setupTabBar() {
         let views = items.compactMap { item in
             let vc = UIStoryboard(name: item.name, bundle: nil).instantiateViewController(withIdentifier: item.identifier)
-
             let tabBarItem = UITabBarItem(title: nil,
                                           image: item.image,
                                           selectedImage: item.selectedImage)
             vc.tabBarItem = tabBarItem
-
-
             return vc
         }
         setViewControllers(views, animated: true)
--- tmbdAPI/Controllers/Detail/DetailsViewController.swift
+++ tmbdAPI/Controllers/Detail/DetailsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
