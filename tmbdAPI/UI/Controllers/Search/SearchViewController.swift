d40c1bb3edbb6a09c6056865dd5a49a2bd6b89df
ealpago <alpagoemre@icloud.com>
2023-06-18 00:15:40 +0300

    SearchVC ready for customization

--- tmbdAPI/UI/Controllers/Search/SearchViewController.swift
+++ tmbdAPI/UI/Controllers/Search/SearchViewController.swift
@@ -9,21 +9,30 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet private weak var searchBar: UISearchBar!
+    @IBOutlet private weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupTableView(with: tableView)
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    private func setupTableView(with tableView: UITableView) {
+        tableView.register(DetailedTableViewCell.nib, forCellReuseIdentifier: DetailedTableViewCell.identifier)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-    */
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: DetailedTableViewCell.identifier, for: indexPath) as? DetailedTableViewCell {
+            return cell
+        }
+        return UITableViewCell()
+    }
 }
