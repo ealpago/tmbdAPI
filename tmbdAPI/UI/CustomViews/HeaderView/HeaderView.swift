b6ae061c5d37277919b2f7e568f1d546a788af3d
ealpago <alpagoemre@icloud.com>
2023-06-17 22:15:16 +0300

    HeaderView customized

--- tmbdAPI/UI/CustomViews/HeaderView/HeaderView.swift
+++ tmbdAPI/UI/CustomViews/HeaderView/HeaderView.swift
@@ -15,11 +15,13 @@ class HeaderView: UIView {
     @IBOutlet private weak var buttonsStackView: UIStackView!
     @IBOutlet private weak var moviesButton: UIButton!
     @IBOutlet private weak var seriesButton: UIButton!
-    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var buttonContainerView: UIView!
+    @IBOutlet private weak var headerMovieButton: UIButton!
 
     //MARK: Properties
     var moviesButtonTapped:()->() = {}
     var seriesButtonTapped:()->() = {}
+    var headerMovieButtonTapped:()->() = {}
 
     //MARK: Init Setup
     override init(frame: CGRect) {
@@ -43,6 +45,8 @@ class HeaderView: UIView {
         seriesButton.layer.borderWidth = 1
         seriesButton.layer.borderColor = UIColor.brown.cgColor
         seriesButton.layer.cornerRadius = moviesButton.frame.height / 2
+        headerMovieButton.layer.borderWidth = 2
+        headerMovieButton.layer.borderColor = UIColor.brown.cgColor
     }
 
     //MARK: Actions
@@ -52,5 +56,9 @@ class HeaderView: UIView {
 
     @IBAction func seriesButtonTapped(_ sender: UIButton) {
         seriesButtonTapped()
+    }
+
+    @IBAction func headerMovieButtonTapped(_ sender: UIButton) {
+        headerMovieButtonTapped()
     }
 }
