ba0b5ac83ce09cb5ec4cbe481eb6f4c9b1e911a3
ealpago <alpagoemre@icloud.com>
2023-06-17 23:32:20 +0300

    UpcomingVC and DetailedTableViewCell ready for customization

--- tmbdAPI/UI/Controllers/Upcoming/UpcomingViewController.swift
+++ tmbdAPI/UI/Controllers/Upcoming/UpcomingViewController.swift
@@ -9,21 +9,29 @@ import UIKit
 
 class UpcomingViewController: UIViewController {
 
+    @IBOutlet private weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupTableView(with: tableView)
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
+extension UpcomingViewController: UITableViewDelegate, UITableViewDataSource {
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupTableView(with tableView: UITableView) {
+        tableView.register(DetailedTableViewCell.nib, forCellReuseIdentifier: DetailedTableViewCell.identifier)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-    */
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: DetailedTableViewCell.identifier, for: indexPath) as? DetailedTableViewCell {
+            return cell
+        }
+        return UITableViewCell()
+    }
 }
--- tmbdAPI/UI/CustomViews/TableViews/DetailedTableView/DetailedTableViewCell.swift
+++ tmbdAPI/UI/CustomViews/TableViews/DetailedTableView/DetailedTableViewCell.swift
@@ -12,19 +12,22 @@ class DetailedTableViewCell: UITableViewCell {
     @IBOutlet weak var mainStackView: UIStackView!
     @IBOutlet weak var leftImageView: UIView!
     @IBOutlet weak var detailImageView: UIImageView!
+    @IBOutlet weak var titlesStackView: UIStackView!
     @IBOutlet weak var detailTitleLabel: UILabel!
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
     @IBOutlet weak var buttonView: UIView!
     @IBOutlet weak var detailButton: UIButton!
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        mainStackView.layer.borderWidth = 2
+        mainStackView.layer.borderColor = UIColor.brown.cgColor
+
+        mainStackView.layer.cornerRadius = 8
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
 }
