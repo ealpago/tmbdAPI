875b7c2edd4c66e6124430955e50bc55e77d6024
ealpago <alpagoemre@icloud.com>
2023-06-21 20:33:49 +0300

    SearchMoviesResponse added

---
+++ tmbdAPI/Models/MoviesResponseModels/SearchedMoviesResponseModel/SearchedMovies.swift
@@ -0,0 +1,31 @@
+//
+//  SearchedMovies.swift
+//  tmbdAPI
+//
+//  Created by Emre Alpago on 21.06.2023.
+//
+
+import Foundation
+
+struct SearchedMovies: Codable {
+
+    let page: Int?
+    let results: [SearchedMoviesResults]?
+    let totalPages: Int?
+    let totalResults: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case page = "page"
+        case results = "results"
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        page = try values.decodeIfPresent(Int.self, forKey: .page)
+        results = try values.decodeIfPresent([SearchedMoviesResults].self, forKey: .results)
+        totalPages = try values.decodeIfPresent(Int.self, forKey: .totalPages)
+        totalResults = try values.decodeIfPresent(Int.self, forKey: .totalResults)
+    }
+}
