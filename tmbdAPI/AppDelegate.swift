be1a8b6307aa3f582a8f1ebdf9fba5af0a004337
ealpago <alpagoemre@icloud.com>
2023-07-02 01:50:12 +0300

    IQKeyboardManager added to SPM

--- tmbdAPI/AppDelegate.swift
+++ tmbdAPI/AppDelegate.swift
@@ -9,6 +9,7 @@ import UIKit
 import CoreData
 import FirebaseCore
 import FirebaseFirestore
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,6 +20,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         FirebaseApp.configure()
         let db = Firestore.firestore()
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+
+
         return true
     }
 
